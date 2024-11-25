9d46a6c231e3dcb652e1193f3a2b79c4deac8891
anuragsingh0024 <anuragsinghrajput450@gmail.com>
2024-11-25 23:53:59 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,77 @@
+const form = document.querySelector('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const cPassword = document.getElementById('confirm-password')
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    validateInputs();
+})
+
+const validateInputs = () => {
+           const usernameValue = username.value.trim();
+           const emailValue = email.value.trim();
+           const passwordValue = password.value.trim();
+           const cPasswordValue = cPassword.value.trim();
+
+           if(usernameValue === ''){
+                  setError(username, "Username is must be required")
+           }
+           else {
+              setSuccess(username)
+           }
+
+           if(emailValue === ''){
+            setError(email, "Email must be required")
+           }
+           else if(!isValidEmail(emailValue)){
+                  setError(email, "Please Enter a valid email")
+           }
+           else {
+            setSuccess(email)
+           }
+
+           if(passwordValue === ''){
+             setError(password, "Password must be required")
+           }
+           else if(passwordValue.length<8){
+               setError(password, "Password must be atleast 8 characters")
+           }
+           else {
+            setSuccess(password)
+           }
+
+          
+          if(cPasswordValue != passwordValue){
+              setError(cPassword, "Password does'nt match")
+          }
+          else {
+           setSuccess(cPassword)
+          }
+}
+
+const setError = (element, message)=>{
+         const inputControl = element.parentElement;
+         const displayError = inputControl.querySelector('.error');
+
+         displayError.textContent = message
+         inputControl.classList.remove('success')
+         inputControl.classList.add('error')
+}
+
+const setSuccess = (element)=> {
+    const inputControl = element.parentElement;
+    const displayError = inputControl.querySelector('.error')
+
+    displayError.textContent = '';
+    inputControl.classList.add('success')
+    inputControl.classList.remove('error')
+}
+
+const  isValidEmail = (email) => {
+    const validateEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
+}
